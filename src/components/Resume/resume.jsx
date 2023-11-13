491629896a56f3f5779f75266ee88dd4a23d252c
chappiii <chappi787@gmail.com>
2023-11-13 14:28:27 +0300

    resume...

--- src/components/Resume/resume.jsx
+++ src/components/Resume/resume.jsx
@@ -2,18 +2,23 @@ import { Document, Page, pdfjs } from "react-pdf";
 import pdf from "../../assets/Kidus Mikael Birhanu.pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import Button from "react-bootstrap/Button";
-
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+import { useEffect } from "react";
 
 function Resume() {
+  // useEffect(() => {
+  //   pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+  // }, []);
+
+  // const downloadResume = () => {
+  //   const link = document.createElement("a");
+  //   link.href = pdf;
+  //   link.download = "Kidus Mikael Birhanu.pdf";
+  //   link.click();
+  // };
+
   return (
-    <div className="text-white font-barlow pt-36 bg-slate-950 bg-left-top bg-cover h-fit w-full">
-      <Button className="bg-slate-800 hover:bg-lime-700 text-white font-bold py-2 mb-4 px-4 rounded inline-flex items-center">
-        Download Resume
-      </Button>
-      <Document file={pdf} className="mx-32">
-        <Page pageNumber={1} />
-      </Document>
+    <div className="text-white font-barlow pt-36 mb-80 bg-slate-950 bg-left-top bg-cover  w-full">
+      <h1>Coming soon...</h1>
     </div>
   );
 }

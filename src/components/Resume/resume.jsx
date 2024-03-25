46ccced531a9707ae3a1c0aa3fef5ac24256eb40
chappiii <chappi787@gmail.com>
2024-03-25 14:07:15 +0300

    resume pdf done

--- src/components/Resume/resume.jsx
+++ src/components/Resume/resume.jsx
@@ -1,26 +1,49 @@
+import { useState, useEffect } from "react";
+import Particle from "../Particle";
+import pdf from "../../assets/Kidus Resume.pdf";
+import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
-import pdf from "../../assets/Kidus Mikael Birhanu.pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-import Button from "react-bootstrap/Button";
-import { useEffect } from "react";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-function Resume() {
-  // useEffect(() => {
-  //   pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-  // }, []);
+const resumeLink =
+  "https://raw.githubusercontent.com/chappiii/chappiii.github.io/master/src/assets/Kidus%20Resume.pdf";
 
-  // const downloadResume = () => {
-  //   const link = document.createElement("a");
-  //   link.href = pdf;
-  //   link.download = "Kidus Mikael Birhanu.pdf";
-  //   link.click();
-  // };
+function ResumeNew() {
+  const [width, setWidth] = useState(1200);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
 
   return (
-    <div className="text-white font-barlow pt-36 mb-80 bg-slate-950 bg-left-top bg-cover  w-full">
-      <h1>Coming soon...</h1>
+    <div className="min-h-screen bg-slate-950 flex flex-col items-center h-[50] justify-center pt-36">
+      <div className="resume-section w-full max-w-4xl mx-auto shadow-lg p-8">
+        <Particle />
+        <div className="flex justify-center py-5">
+          <a
+            href={pdf}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn-primary inline-flex items-center px-6 py-2 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
+          >
+            <AiOutlineDownload className="" />
+            Download CV
+          </a>
+        </div>
+
+        <div className="resume flex justify-center">
+          <Document file={resumeLink}>
+            <Page
+              pageNumber={1}
+              renderTextLayer={false}
+              scale={width > 786 ? 1.5 : 0.6}
+            />
+          </Document>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default Resume;
+export default ResumeNew;
